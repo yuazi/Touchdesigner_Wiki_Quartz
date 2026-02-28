5ceb2a4ec0cf78ee8ef56a0dbceccfce5070cef2
YusufAbdulAziz <st171502@stud.uni-stuttgart.de>
2026-02-28 22:36:41 +0100

    Quartz sync: Feb 28, 2026, 10:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618",
+          light: "#1a1a2e",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
