c8bb96c8a70c8955c2413f54f65e275cf396a378
YusufAbdulAziz <st171502@stud.uni-stuttgart.de>
2026-02-28 11:51:07 +0100

    Quartz sync: Feb 28, 2026, 11:51 AM

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -23,6 +23,7 @@ import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
+import LorenzBackground from "./LorenzBackground"
 
 export {
   ArticleTitle,
@@ -50,4 +51,5 @@ export {
   Comments,
   Flex,
   ConditionalRender,
+  LorenzBackground,
 }
