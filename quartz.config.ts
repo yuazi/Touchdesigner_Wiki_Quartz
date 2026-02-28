4512062c68baa3ae9f43ea415058a04f01623936
YusufAbdulAziz <st171502@stud.uni-stuttgart.de>
2026-03-01 00:04:02 +0100

    Quartz sync: Mar 1, 2026, 12:04 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "yuazi.github.io/touchdesignerwiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
