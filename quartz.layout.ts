bbab029ebc6596f7df63154817f8a57cf0f4b3a0
YusufAbdulAziz <st171502@stud.uni-stuttgart.de>
2026-03-05 21:18:58 +0100

    Quartz sync: Mar 5, 2026, 9:18 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/yuazi/_y_usagewiki",
-      About: "/about",
+      About: "/_y_usagewiki/about",
     },
   }),
 }
